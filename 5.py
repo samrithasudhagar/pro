fcf90e1b5b2bcac783e8fcd0cf323cb356c0d9ad
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-02-18 21:18:20 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,21 @@
+n,a,b=map(int,input().split())
+
+if n%2!=0:
+    print("NO")
+elif n==2 and a==1 and b==1:
+    print("YES")
+else:
+    k=n//2
+    for i in range(1,n):
+        for j in range(1,n):
+            if (a*i)+(b*j)==k:
+                flag=0
+                break
+            else:
+                flag=1
+        if flag==0:
+            break
+    if flag==0:
+        print("YES")
+    else:
+        print("NO")
