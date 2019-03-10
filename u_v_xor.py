382ade390cdc598987e7dabbaa2b2c8a5c3e42b5
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-03-10 18:39:50 +0530

    Create u_v_xor.py

---
+++ u_v_xor.py
@@ -0,0 +1,14 @@
+n,q=map(int,input().split())
+l=list(map(int,input().split()))
+a=[]
+while q!=0:
+    s=0
+    u,v=map(int,input().split())
+    x=0
+    for i in range(u-1,v):
+        x=x^l[i]
+        if i==v-1:
+            a.append(x)
+    q=q-1
+for i in range(0,len(a)):
+    print(a[i])
