9f803d2057047df4e22ba812cea034096288a5ff
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-27 13:11:52 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,15 +1,15 @@
 n=int(input())
 l=list(map(int,input().split()))
-l.sort()
 p=[]
 for i in l:
     p.append(1)
 for i in range(len(l)):
     if i==0:
-        p[0]=p[0]
-    else:
-        if l[i]==l[i-1]:
-            p[i]=p[i-1]
+        if l[0]>l[1]:
+            p[0]=p[0]+1
         else:
+            p[0]=p[0]
+    else:
+        if l[i]>l[i-1]:
             p[i]=p[i-1]+1
 print(sum(p))
