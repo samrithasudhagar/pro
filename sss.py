4186ec84d7f1256c09d908c757a4b49a27fc3234
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-06-20 15:12:22 +0530

    Create sss.py

---
+++ sss.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+a_ha=0
+b_ha=0
+l.sort(reverse=True)
+for i in l:
+  m=a_ha+i
+  if b_ha>m:
+    a_ha=m
+  else:
+    a_ha=b_ha
+    b_ha=m
+print(a_ha,b_ha)
